9367f34ee0063038278085e292d76353e13bcbf4
Navid079 <navid.naser.079@gmail.com>
2022-12-09 19:53:26 +0330

    reading and compiling model added

--- index.js
+++ index.js
@@ -7,10 +7,22 @@ const path = require("path");
 // Module components
 const dbConnect = require("./connections/db-connection");
 const args = require("./inputs/args");
+const modelCompiler = require("./inputs/model");
 
-const argv = args(process.argv);
-const { username, password, host, port, database } = argv;
+// start point
+const start = async () => {
+  const argv = args(process.argv);
+  const { username, password, host, port, database, model } = argv;
 
-dbConnect({ username, password, host, port, database }).then(result => {
-  console.log(result.url);
-});
+  const compiledModel = modelCompiler(model);
+
+  const mongooseConnection = await dbConnect({
+    username,
+    password,
+    host,
+    port,
+    database,
+  });
+};
+
+start();
---
+++ inputs/model.js
@@ -0,0 +1,31 @@
+const { promises: fs, existsSync, readFileSync } = require("fs");
+const path = require("path");
+
+const getAbsolutePath = modelPath => {
+  if (path.win32.isAbsolute(modelPath)) {
+    return modelPath;
+  } else {
+    return path.win32.join(process.cwd(), modelPath);
+  }
+};
+
+const isFileExists = modelPath => existsSync(modelPath);
+
+const compileModel = content => {
+  const lines = content.split("\n");
+  const modelArray = lines.map(line => line.split("="));
+  if (modelArray.find(item => item.length !== 2)) throw new Error("2");
+
+  const modelObject = Object.fromEntries(modelArray);
+
+  return modelObject;
+};
+
+module.exports = modelPath => {
+  const absPath = getAbsolutePath(modelPath);
+  console.log(absPath)
+  if (!isFileExists(absPath)) throw new Error("1");
+  const fileContent = readFileSync(absPath, "ascii");
+
+  return compileModel(fileContent);
+};
