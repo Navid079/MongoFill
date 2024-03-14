7f36ead019ed5b440e7b88903bd23d5e0ca9523c
Navid Naseri <navid.naseri.079@gmail.com>
2024-03-14 16:10:42 +0330

    added usage message and changed license

--- messages/info.js
+++ messages/info.js
@@ -1,3 +1,8 @@
 module.exports = {
-  argsUsage: "usage here",
+  argsUsage:
+    "Usage: \n" +
+    "mongoseed [-u username] [-w password]\n" +
+    "-h host -p port\n" +
+    "[-d database] -c collection\n" +
+    "-m model [-n count] [-s save [-s save ...]] [--save-id]",
 };
